adfe7ce1d80889e8eccf9806f8552af8ecedb51a
yanziqi <yanziq@caifu500.com>
2014-12-22 10:19:58 +0800

    测试添、删、查、改 Signed-off-by: yanziqi <yanziq@caifu500.com>

--- src/main/java/com/wyrz/shop/controller/ShopController.java
+++ src/main/java/com/wyrz/shop/controller/ShopController.java
@@ -23,12 +23,34 @@ public class ShopController {
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index() {
-		// 注解方式
 		// List<Guarantee> guarantees = guaranteeAnnotationService.queryAll();
+		// GuaranteeQuery query = new GuaranteeQuery();
+		// query.setGuaranteeId(1);
+		// query.setGuaranteeName("ccc");
+		// Pageable pageable = new PageRequest(1, 2);
+		// List<Guarantee> guarantees = guaranteeAnnotationService.queryList(query, pageable);
+		// List<Integer> idList = guaranteeAnnotationService.queryAllId();
+		// List<Guarantee> guarantees = guaranteeAnnotationService.queryByIdList(idList);
+		// List<Integer> idList = guaranteeAnnotationService.queryIdList(query);
+		// List<Guarantee> guarantees = guaranteeAnnotationService.queryList(query);
+		// Guarantee guarantee = guaranteeAnnotationService.queryOne(query);
 		// Guarantee guarantee = guaranteeAnnotationService.queryById(1);
-		// Xml方式
-		// List<Guarantee> guarantees = guaranteeXmlService.queryAll();
-		// Guarantee guarantee = guaranteeXmlService.queryById(1);
+		// Long count = guaranteeAnnotationService.queryCount(query);
+		// Guarantee guarantee = new Guarantee();
+		// guarantee.setGuaranteeName("yanziqi");
+		// guarantee.setGuaranteeTel("123455");
+		// guaranteeAnnotationService.insert(guarantee);
+		//
+		// List<Guarantee> guaranteeList = new ArrayList<Guarantee>();
+		// guaranteeList.add(guarantee);
+		// guaranteeList.add(guarantee);
+		// guaranteeAnnotationService.insertInBatch(guaranteeList);
+		// guarantee.setGuaranteeName("ritchieyan");
+		// guaranteeAnnotationService.updateById(guarantee);
+		// guaranteeAnnotationService.deleteById(10);
+		// guaranteeAnnotationService.delete(guarantee);
+		// System.out.println(count);
+		// System.out.println(idList);
 		// System.out.println(guarantees);
 		// System.out.println(guarantee);
 		return "index";
